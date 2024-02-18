af7e83d2103309a40c915e7fac2d88c538fdf45d
yeasinshakil <shakilrock1998@gmail.com>
2024-02-18 21:41:46 +0600

    other aritcles section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Articles from './components/Articles'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Posts from './components/Post'
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Posts />
+      <Articles />
     </>
   )
 }
